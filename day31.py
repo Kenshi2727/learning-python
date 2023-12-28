b1228cd8b346d372677b6086a366c67c822280fc
Kenshi2727 <abhishekmathurofficial@gmail.com>
2023-12-28 10:05:55 +0530

    learbed sets in python

---
+++ day31.py
@@ -0,0 +1,14 @@
+# sets in python
+s={1,2,3,4,4,3,3,3,3,3,3,3,2,2,2,11,1,2,23}
+print(s)
+
+
+kenshi={}
+print(type(kenshi))
+
+mathur=set() #correct syntax to make an empty set
+print(type(mathur))
+
+#accessing elements of a set
+for i in s:
+    print(i)
