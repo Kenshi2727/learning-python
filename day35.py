06db73e7a0a0d4bce8c0b48976d447713433043f
Kenshi2727 <abhishekmathurofficial@gmail.com>
2023-12-30 19:40:08 +0530

    learned about using else with for loop

---
+++ day35.py
@@ -0,0 +1,15 @@
+# for loop with else
+for i in range(5):
+    print(i)
+else:
+    print("Sorry no i!")
+
+for i in range(5):
+    print(i)
+    if i==4:
+     break;
+else:
+    print("Sorry no i!")  # will not be executed!
+
+print("end of code snippet")
+
