fdd894492374585170b824cba7d7546394075b85
Kenshi2727 <abhishekmathurofficial@gmail.com>
2024-02-08 09:07:35 +0530

    shorthand if else

---
+++ day41.py
@@ -0,0 +1,14 @@
+a = 330
+b = 3303
+
+# print("A") if a>b else print("=") if a==b else print("B")
+
+if a == 330:
+    print("Kenshi is swordsman")
+else:
+    print("Kenshi is Japanese Kshatriya")
+
+# Using shorthand if else
+print("Kenshi is swordsman") if a == 330 else print("Kenshi is Japanese Kshatriya")
+
+# Same output corresponds to the correct syntax
